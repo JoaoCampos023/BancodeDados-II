3020a4a1f2911aaf7c7dfe909313e4d3e8f54a8b
Joao Vitor Campos <joaovcampos023@gmail.com>
2025-08-15 22:03:30 -0300

    Aula 15/08

---
+++ Atividades/Aula 03/Imobiliaria/Imobiliaria/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Imobiliaria.Models;
+
+namespace Imobiliaria.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Imovel> Imoveis { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configurações adicionais do modelo podem ser feitas aqui
+            modelBuilder.Entity<Imovel>().ToTable("Imovel");
+        }
+    }
+}
---
+++ Atividades/Aula 03/Imobiliaria/Imobiliaria/Models/Imobiliaria.cs
@@ -0,0 +1,12 @@
+﻿namespace Imobiliaria.Models
+{
+    public class Imovel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } 
+        public int NumberRoom { get; set; }
+        public decimal SalePrice { get; set; }
+
+    }
+}
