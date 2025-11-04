5b3e16261a9741e9fde406cec55202f82b7a4ff6
Joao Vitor Campos <joaovcampos023@gmail.com>
2025-11-04 16:23:42 -0300

    Finalizado 2

--- Atividades/Projeto/TransporteAereo/TransporteAereo/SistemaTransporteAereo.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TransporteAereo.SistemaTransporteAereo.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
